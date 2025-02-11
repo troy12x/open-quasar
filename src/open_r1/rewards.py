18280e6f10303412a8783b0e4dc234c4127b3b87
Eiad Gomaa <61633360+troy12x@users.noreply.github.com>
2025-02-11 14:33:42 +0200

    Update rewards.py

--- src/open_r1/rewards.py
+++ src/open_r1/rewards.py
@@ -51,7 +51,7 @@ def accuracy_reward(completions, solution, **kwargs):
 
 def format_reward(completions, **kwargs):
     """Reward function that checks if the completion has a specific format."""
-    pattern = r"<\|begin_of_thought\|>.*?<\|end_of_thought\|><\|begin_of_solution\|>.*?<\|end_of_solution\|>$"
+    pattern = r"^<think>.*?</think>\s*<answer>.*?</answer>$"
     completion_contents = [completion[0]["content"] for completion in completions]
     matches = [re.match(pattern, content, re.DOTALL | re.MULTILINE) for content in completion_contents]
     return [1.0 if match else 0.0 for match in matches]
